9e6edfe3dca224f0c938b71b4ea86e3228dcbad9
JunYoung <twinjyjh2@gmail.com>
2021-11-12 18:26:19 +0900

    21.11.12 TS Study content (Input)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    setValue(e.currentTarget.value);
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Hello", value);
+    setValue("");
+  }
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="blue"/>
-      <Circle bgColor="tomato" text="tomato color"/>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username" />
+        <button type="submit">Log in</button>
+      </form>
     </div>
   );
 }
