0600cb5494df2cbfcfbf5e6a14e19abf4fd6b660
JunYoung <twinjyjh2@gmail.com>
2021-11-12 19:11:36 +0900

    21.11.12 Study content (styled-somponents: Theme)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value);
-    setValue(e.currentTarget.value);
-  }
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Hello", value);
-    setValue("");
-  }
+  const Container = styled.div`
+    background-color: ${porps => porps.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+    color: ${porps => porps.theme.textColor};
+  `;
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} type="text" placeholder="username" />
-        <button type="submit">Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Wow</H1>
+    </Container>
   );
 }
 
