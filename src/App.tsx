1676fc2d4ed55021ad3d5a407f3856e4f436a712
JunYoung <twinjyjh2@gmail.com>
2021-11-14 22:15:14 +0900

    21.11.14 Study content (React Hook Form)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400&display=swap');
@@ -68,6 +69,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
