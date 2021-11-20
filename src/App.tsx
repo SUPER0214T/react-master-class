29d80aa026eecea78d49b0c8a0f0b6ffe8682208
JunYoung <twinjyjh2@gmail.com>
2021-11-20 22:17:14 +0900

    21.11.20 Study content (Framer Motion: AnimatePresence)

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,53 @@
-import { motion, useMotionValue, useTransform, Variants } from 'framer-motion';
-import React, { useEffect, useRef } from 'react';
+import {
+	motion,
+	useMotionValue,
+	useTransform,
+	Variants,
+	useViewportScroll,
+	AnimatePresence,
+} from 'framer-motion';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled(motion.div)`
-	display: flex;
-	justify-content: center;
-	align-items: center;
+	text-align: center;
 	width: 100vw;
 	height: 100vh;
 	background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
-	width: 200px;
+	margin: 0 auto;
+	width: 400px;
 	height: 200px;
 	background-color: white;
 	border-radius: 40px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const boxVariant: Variants = {
+	initial: { scale: 0, opacity: 0 },
+	visible: { scale: 1, opacity: 1, rotateZ: 360 },
+	leaving: { scale: 0, opacity: 0, y: 50 },
+};
+
 function App() {
-	const x = useMotionValue(0);
-	const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-	const gradient = useTransform(
-		x,
-		[-800, 0, 800],
-		[
-			'linear-gradient(135deg, rgb(137, 240, 40), rgb(70, 81, 238))',
-			'linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))',
-			'linear-gradient(135deg, rgb(235, 212, 10), rgb(240, 101, 21))',
-		]
-	);
+	const [showing, setShowing] = useState(true);
+	const toggleShowing = () => setShowing((prev) => !prev);
 
 	return (
-		<Wrapper style={{ background: gradient }}>
-			<Box style={{ x, rotateZ }} drag dragSnapToOrigin />
+		<Wrapper>
+			<button onClick={toggleShowing}>Toggle</button>
+			<AnimatePresence>
+				{showing ? (
+					<Box
+						variants={boxVariant}
+						initial="initial"
+						animate="visible"
+						exit="leaving"
+					/>
+				) : null}
+			</AnimatePresence>
 		</Wrapper>
 	);
 }
