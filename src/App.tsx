a6897e75b0e29315659fde8a6efd22232f89b1f2
JunYoung <twinjyjh2@gmail.com>
2021-11-22 21:07:25 +0900

    21.11.22 Study content (Framer Motion: card animation)

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ import styled from 'styled-components';
 const Wrapper = styled(motion.div)`
 	display: flex;
 	align-items: center;
-	justify-content: center;
+	flex-wrap: nowrap;
+	justify-content: space-around;
 	text-align: center;
 	width: 100vw;
 	height: 100vh;
@@ -20,64 +21,129 @@ const Wrapper = styled(motion.div)`
 	position: relative;
 `;
 
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-gap: 10px;
+	width: 50vw;
+
+	div:nth-child(1) {
+		grid-column: 1 / 3;
+	}
+
+	div:nth-child(4) {
+		grid-column: 2 / 4;
+	}
+`;
+
 const Box = styled(motion.div)`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-size: 2rem;
-	width: 350px;
-	height: 150px;
+	height: 200px;
 	background-color: white;
 	border-radius: 10px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-	position: absolute;
-	top: 100px;
+	cursor: pointer;
 `;
 
-const boxVariant: Variants = {
-	invisible: (isBack: Boolean) => ({
-		x: isBack ? -500 : 500,
-		scale: 0,
-		opacity: 0,
-	}),
-	visible: { x: 0, scale: 1, opacity: 1, transition: { duration: 0.35 } },
-	exit: (isBack: Boolean) => ({
-		x: isBack ? 500 : -500,
-		rotateX: 180,
-		scale: 0,
-		opacity: 0,
-		transition: { duration: 0.35 },
-	}),
+const ModalWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+
+	width: 100%;
+	height: 100%;
+
+	.box {
+		position: relative;
+		width: 500px;
+		height: 300px;
+		z-index: 1;
+		cursor: auto;
+	}
+
+	.modalClose {
+		transition: background-color 0.3s ease-in-out;
+
+		&:hover {
+			background-color: red;
+		}
+	}
+`;
+
+const Overlay = styled(motion.div)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	cursor: pointer;
+`;
+
+const overlayVariants: Variants = {
+	initial: { backgroundColor: 'rgba(0, 0, 0, 0)' },
+	animate: {
+		backgroundColor: 'rgba(0, 0, 0, 0.5)',
+	},
+	exit: { backgroundColor: 'rgba(0, 0, 0, 0)' },
 };
 
+const CloseButton = styled(motion.button)`
+	position: absolute;
+	top: 5px;
+	right: 5px;
+	width: 30px;
+	height: 30px;
+	background-color: pink;
+	border: none;
+	outline: none;
+	cursor: pointer;
+	border-radius: 50%;
+`;
+
 function App() {
-	const [back, setBack] = useState(false); // Change the slider direction.
-	const [visible, setVisible] = useState(1);
-	const nextVisible = () => {
-		setBack(false);
-		setVisible((prev) => (prev === 10 ? 1 : prev + 1));
-	};
-	const prevVisible = () => {
-		setBack(true);
-		setVisible((prev) => (prev === 1 ? 10 : prev - 1));
-	};
+	const [id, setId] = useState<null | string>(null);
 
 	return (
 		<Wrapper>
-			<AnimatePresence custom={back}>
-				<Box
-					key={visible}
-					variants={boxVariant}
-					initial="invisible"
-					animate="visible"
-					exit="exit"
-					custom={back}
-				>
-					{visible}
-				</Box>
+			<Grid>
+				{[1, 2, 3, 4].map((el, index) => {
+					return (
+						<Box
+							onClick={() => setId(el + '')}
+							key={index}
+							layoutId={el + ''}
+						/>
+					);
+				})}
+			</Grid>
+			<AnimatePresence>
+				{id ? (
+					<ModalWrapper>
+						<Overlay
+							onClick={(e) => {
+								setId(null);
+							}}
+							className="overlay"
+							variants={overlayVariants}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+						></Overlay>
+						<Box className="box" layoutId={id}>
+							<CloseButton
+								onClick={(e) => {
+									setId(null);
+								}}
+								className="modalClose"
+								type="button"
+							>
+								x
+							</CloseButton>
+						</Box>
+					</ModalWrapper>
+				) : null}
 			</AnimatePresence>
-			<button onClick={nextVisible}>Next</button>
-			<button onClick={prevVisible}>Prev</button>
 		</Wrapper>
 	);
 }
