9d816265bcc8a28a91ce07e7b706b5d04122c4d7
JunYoung <twinjyjh2@gmail.com>
2021-12-06 17:01:23 +0900

    Update search input of Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
 
 function App() {
-	return null;
+	return (
+		<BrowserRouter>
+			<Header />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/tv">
+					<Tv />
+				</Route>
+				<Route path="/search">
+					<Search />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
